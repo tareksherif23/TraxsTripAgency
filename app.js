63fd2e192ced3338e0bdbf45631bf2482b30f539
Tarek <tareksherif23@yahoo.com>
2021-12-31 16:34:28 +0200

    brief_func_testing

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: "Hello there !", app: "TraxsTrips" });
+app.use(express.json())
+
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: "Hello there !", app: "TraxsTrips" });
+// })
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+
+
+app.get('/api/v1/tours', (req, res) => {
+  res.json({
+    status: 'sucess',
+    results: tours.length,
+    data: { tours }
+  })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+  const newID = tours[tours.length - 1].id + 1
+  const newTour = Object.assign({ id: newID }, req.body)
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours), err => {
+      res.status(201).json(
+        {
+          status: "Success!",
+          data: { tour: newTour }
+        }
+      )
+    })
 })
 
 const port = 3000;
